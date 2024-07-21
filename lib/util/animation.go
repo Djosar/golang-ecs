cd15558bea19578549687050a73f43d41433bc31
lukas.hartmann <lukas.hartmann@adabay.rocks>
2024-07-21 17:53:46 +0200

    chore: more godoc comments

--- lib/util/animation.go
+++ lib/util/animation.go
@@ -6,6 +6,8 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// Animation represents a sequence of frames for an animation. It contains the
+// frames, marker, counter, animation speed, and the current frame index.
 type Animation struct {
 	Frames         []*ebiten.Image
 	Marker         int
@@ -14,6 +16,17 @@ type Animation struct {
 	FrameIndex     int
 }
 
+// NewAnimation creates and returns a new Animation instance.
+//
+// Parameters:
+//
+//	frames ([]*ebiten.Image): The frames of the animation.
+//	marker (int): The marker indicating the end of the animation sequence.
+//	speed (int): The speed of the animation.
+//
+// Returns:
+//
+//	*Animation: A pointer to the newly created Animation instance.
 func NewAnimation(frames []*ebiten.Image, marker int, speed int) *Animation {
 	return &Animation{
 		Frames:         frames,
@@ -24,11 +37,28 @@ func NewAnimation(frames []*ebiten.Image, marker int, speed int) *Animation {
 	}
 }
 
+// GetCurrentFrame returns the current frame of the animation.
+//
+// Returns:
+//
+//	*ebiten.Image: The current frame of the animation.
 func (a *Animation) GetCurrentFrame() *ebiten.Image {
-
 	return a.Frames[a.FrameIndex]
 }
 
+// GenerateFrames generates a sequence of frames from a sprite sheet based on the
+// provided tile coordinates.
+//
+// Parameters:
+//
+//	sprite (*ebiten.Image): The sprite sheet image.
+//	tileWidth (int): The width of each tile/frame.
+//	tileHeight (int): The height of each tile/frame.
+//	tileCoordinates ([]*Coordinate[int]): The coordinates of the tiles to be extracted.
+//
+// Returns:
+//
+//	[]*ebiten.Image: A slice of ebiten.Image pointers representing the frames.
 func GenerateFrames(
 	sprite *ebiten.Image,
 	tileWidth, tileHeight int,
